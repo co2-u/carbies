ceb3a9878b73ab1a790d7c7c89753e27e26c2ed7
acamacho4957 <acamacho4957@gmail.com>
2019-07-17 11:22:30 -0700

    User persists between app restarts and settings fragment so user can log out

---
+++ app/src/main/java/com/example/carbonfootprinttracker/fragments/SettingsFragment.java
@@ -0,0 +1,47 @@
+package com.example.carbonfootprinttracker.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.carbonfootprinttracker.MainActivity;
+import com.example.carbonfootprinttracker.R;
+import com.parse.ParseUser;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SettingsFragment extends Fragment {
+
+    private static final String TAG = "SettingsFragment";
+
+    @BindView(R.id.btLogout) Button btLogout;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        ButterKnife.bind(this, view);
+
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                Log.d(TAG, "Logged out successfully");
+                Intent intent = new Intent(getContext(), MainActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+    }
+}
