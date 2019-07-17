cdecf3d10aa46a33126fb1425676891537a67f82
Jennifer Tian Zhuge <jennytzhuge@gmail.com>
2019-07-17 14:44:42 -0700

    new fragment_confirmation_screen

--- app/src/main/java/com/example/carbonfootprinttracker/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/carbonfootprinttracker/fragments/ComposeFragment.java
@@ -1,6 +1,21 @@
 package com.example.carbonfootprinttracker.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.carbonfootprinttracker.R;
+
 public class ComposeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mode_of_transportation, container, false);
+    }
 }
