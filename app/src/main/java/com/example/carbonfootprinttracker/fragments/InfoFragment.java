1b0f502a0834416c31e6791cb3db295bca1477a0
Jennifer Tian Zhuge <jennytzhuge@gmail.com>
2019-07-17 11:26:05 -0700

    configured the infoFragment - the autolink works also made an xml file for the mode of transportation

--- app/src/main/java/com/example/carbonfootprinttracker/fragments/InfoFragment.java
+++ app/src/main/java/com/example/carbonfootprinttracker/fragments/InfoFragment.java
@@ -1,6 +1,27 @@
 package com.example.carbonfootprinttracker.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.example.carbonfootprinttracker.R;
+
+//TODO butterknife things
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class InfoFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_information, container, false);
+    }
 }
+
