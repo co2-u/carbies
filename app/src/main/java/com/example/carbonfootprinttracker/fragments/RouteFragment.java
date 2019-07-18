5bfcd7823d21c6eedec8942de8e092c17fea0648
acamacho4957 <acamacho4957@gmail.com>
2019-07-18 12:58:26 -0700

    User can see multiple routes, added markers, user can select route.

--- app/src/main/java/com/example/carbonfootprinttracker/fragments/RouteFragment.java
+++ app/src/main/java/com/example/carbonfootprinttracker/fragments/RouteFragment.java
@@ -16,11 +16,14 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.carbonfootprinttracker.R;
+import com.example.carbonfootprinttracker.models.Route;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -37,11 +40,13 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class RouteFragment extends Fragment implements OnMapReadyCallback {
+public class RouteFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnPolylineClickListener {
     private static final String TAG = "RouteFragment";
+    private static final Integer ZOOM_LEVEL = 12;
 
     private GoogleMap mGoogleMap;
     private GeoApiContext mGeoApiContext = null;
+    private ArrayList<Route> mRoutes = new ArrayList<>();
 
     @BindView(R.id.mapView) MapView mMapView;
     @BindView(R.id.btSetRoute) Button btSetRoute;
@@ -72,6 +77,10 @@ public class RouteFragment extends Fragment implements OnMapReadyCallback {
                 final String start = etStart.getText().toString();
                 final String end = etEnd.getText().toString();
 
+                // clear map and mRoutes before adding new routes
+                mGoogleMap.clear();
+                mRoutes.clear();
+
                 calculateDirections(start, end);
             }
         });
@@ -80,6 +89,7 @@ public class RouteFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
+        mGoogleMap.setOnPolylineClickListener(this);
         mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
 
         // Add a marker in Sydney, Australia, and move the camera.
@@ -97,6 +107,7 @@ public class RouteFragment extends Fragment implements OnMapReadyCallback {
         directions.origin(
                 startLocation
         );
+
         Log.d(TAG, "calculateDirections: destination: " + endLocation);
         directions.destination(endLocation).setCallback(new PendingResult.Callback<DirectionsResult>() {
             @Override
@@ -106,7 +117,23 @@ public class RouteFragment extends Fragment implements OnMapReadyCallback {
                 Log.d(TAG, "calculateDirections: distance: " + result.routes[0].legs[0].distance);
                 Log.d(TAG, "calculateDirections: geocodedWayPoints: " + result.geocodedWaypoints[0].toString());
 
+                // Start and End LatLng coordinates and user-friendly addresses
+                final com.google.maps.model.LatLng startLatLng = result.routes[0].legs[0].startLocation;
+                final com.google.maps.model.LatLng endLatLng = result.routes[0].legs[0].endLocation;
+                final String startAddress = result.routes[0].legs[0].startAddress;
+                final String endAddress= result.routes[0].legs[0].endAddress;
+
+                // Get northeast and southwest LatLngBounds to use for centering camera
+                final com.google.maps.model.LatLng preNE = result.routes[0].bounds.northeast;
+                final com.google.maps.model.LatLng preSW = result.routes[0].bounds.southwest;
+                final LatLng northeast = new LatLng(preNE.lat, preNE.lng);
+                final LatLng southwest = new LatLng(preSW.lat, preSW.lng);
+                final LatLngBounds route = new LatLngBounds(southwest, northeast);
+
                 addPolylinesToMap(result);
+                centerCameraOn(route);
+                addMarker(startLatLng.lat, startLatLng.lng, startAddress, BitmapDescriptorFactory.HUE_RED);
+                addMarker(endLatLng.lat, endLatLng.lng, endAddress, BitmapDescriptorFactory.HUE_GREEN);
             }
 
             @Override
@@ -118,29 +145,48 @@ public class RouteFragment extends Fragment implements OnMapReadyCallback {
     }
 
     private void addPolylinesToMap(final DirectionsResult result){
+        // Must run on main thread
         new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override
             public void run() {
                 Log.d(TAG, "run: result routes: " + result.routes.length);
-
-                for(DirectionsRoute route: result.routes){
+                // Iterate through the available routes
+                for (DirectionsRoute route: result.routes){
                     Log.d(TAG, "run: leg: " + route.legs[0].toString());
+                    // This loops through all com.google.maps.model.LatLng coordinates of ONE polyline
+                    // and makes them LatLng coordinates
                     List<com.google.maps.model.LatLng> decodedPath = PolylineEncoding.decode(route.overviewPolyline.getEncodedPath());
-
                     List<LatLng> newDecodedPath = new ArrayList<>();
-
-                    // This loops through all the LatLng coordinates of ONE polyline.
-                    for(com.google.maps.model.LatLng latLng: decodedPath){
-                        newDecodedPath.add(new LatLng(
-                                latLng.lat,
-                                latLng.lng
-                        ));
+                    for (com.google.maps.model.LatLng latLng: decodedPath){
+                        newDecodedPath.add(new LatLng(latLng.lat, latLng.lng));
                     }
+                    // Add polyline to the map
                     Polyline polyline = mGoogleMap.addPolyline(new PolylineOptions().addAll(newDecodedPath));
-                    polyline.setColor(ContextCompat.getColor(getActivity(), R.color.colorRed));
+                    polyline.setColor(ContextCompat.getColor(getActivity(), R.color.colorGrey));
                     polyline.setClickable(true);
-
+                    mRoutes.add(new Route(polyline, route.legs[0]));
                 }
+            }
+        });
+    }
+
+    private void centerCameraOn(LatLngBounds bounds) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bounds.getCenter(), ZOOM_LEVEL));
+            }
+        });
+    }
+
+    private void addMarker(double lat, double lng, String name, float color) {
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                final LatLng markerLocation = new LatLng(lat,lng);
+                mGoogleMap.addMarker(new MarkerOptions().position(markerLocation)
+                                                        .title(name)
+                                                        .icon(BitmapDescriptorFactory.defaultMarker(color)));
             }
         });
     }
@@ -161,5 +207,17 @@ public class RouteFragment extends Fragment implements OnMapReadyCallback {
     public void onDestroy() {
         super.onDestroy();
         mMapView.onDestroy();
+    }
+
+    @Override
+    public void onPolylineClick(Polyline polyline) {
+        polyline.setColor(ContextCompat.getColor(getActivity(), R.color.colorSkyBlue));
+        polyline.setZIndex(1); // brings polyline to front
+        for (Route route: mRoutes) {
+            if (!route.getPolyline().getId().equals(polyline.getId())) {
+                route.getPolyline().setColor(ContextCompat.getColor(getActivity(), R.color.colorGrey));
+                route.getPolyline().setZIndex(0); // brings polyline to front
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/carbonfootprinttracker/models/Route.java
@@ -0,0 +1,32 @@
+package com.example.carbonfootprinttracker.models;
+
+import com.google.android.gms.maps.model.Polyline;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.Distance;
+import com.google.maps.model.Duration;
+
+public class Route {
+    private Polyline polyline;
+    private DirectionsLeg leg;
+
+    public Route(Polyline polyline, DirectionsLeg leg) {
+        this.polyline = polyline;
+        this.leg = leg;
+    }
+
+    public Polyline getPolyline() {
+        return polyline;
+    }
+
+    public DirectionsLeg getLeg() {
+        return leg;
+    }
+
+    public Duration getDuration() {
+        return leg.duration;
+    }
+
+    public Distance getDistance() {
+        return  leg.distance;
+    }
+}
