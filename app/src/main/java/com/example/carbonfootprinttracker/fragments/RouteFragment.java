1a93b3bd8660c9eb4de0268b1b59ed0ac3fc77fe
Jennifer Tian Zhuge <jennytzhuge@gmail.com>
2019-07-17 17:05:34 -0700

    linked up the buttons in the Compose Fragment to go to the RouteFragment once clicked

--- app/src/main/java/com/example/carbonfootprinttracker/fragments/RouteFragment.java
+++ app/src/main/java/com/example/carbonfootprinttracker/fragments/RouteFragment.java
@@ -1,6 +1,22 @@
 package com.example.carbonfootprinttracker.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.carbonfootprinttracker.R;
+
 public class RouteFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Defines(inflates) the xml file for the fragment
+        return inflater.inflate(R.layout.fragment_route, container, false);
+    }
 }
