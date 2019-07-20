8c8b80300521a0744168f05d9cdfa235c70abf70
Jennifer Tian Zhuge <jennytzhuge@gmail.com>
2019-07-19 18:10:56 -0700

    added gas car dialog

--- app/src/main/java/com/example/carbonfootprinttracker/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/carbonfootprinttracker/fragments/ComposeFragment.java
@@ -71,7 +71,7 @@ public class ComposeFragment extends Fragment {
         btnGasCar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goRoute(TransportationMode.Car);
+                showGasCarDialog();
             }
         });
 
@@ -135,5 +135,16 @@ public class ComposeFragment extends Fragment {
         ElectricCarDialog.setArguments(args);
 
         ElectricCarDialog.show(fm, "compose_fragment");
+    }
+
+    public void showGasCarDialog(){
+        GasCarDialogFragment GasCarDialog = new GasCarDialogFragment();
+        Bundle args = new Bundle();
+        Carbie carbie = new Carbie();
+        carbie.setTransportation(TransportationMode.eCar.toString());
+        args.putParcelable("carbie", carbie);
+        GasCarDialog.setArguments(args);
+
+        GasCarDialog.show(fm, "compose_fragment");
     }
 }
