9259a3e83cab49fb869f6a97f4be63403b4038bd
acamacho4957 <acamacho4957@gmail.com>
2019-07-25 12:14:30 -0700

    Request GPS permission on LiveRouteFragment

--- app/src/main/java/com/example/carbonfootprinttracker/fragments/LiveRouteFragment.java
+++ app/src/main/java/com/example/carbonfootprinttracker/fragments/LiveRouteFragment.java
@@ -1,24 +1,26 @@
 package com.example.carbonfootprinttracker.fragments;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.example.carbonfootprinttracker.Manifest;
 import com.example.carbonfootprinttracker.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.GeoApiContext;
 
 import butterknife.BindView;
@@ -26,15 +28,21 @@ import butterknife.ButterKnife;
 
 public class LiveRouteFragment extends Fragment implements OnMapReadyCallback {
     private static final String TAG = "LiveRouteFragment";
+    private static final int REQUEST_FINE_LOCATION = 0;
 
     private GoogleMap mGoogleMap;
     private GeoApiContext mGeoApiContext = null;
     private FragmentManager fragmentManager;
+    private Context context;
 
-    @BindView(R.id.btStart) Button btStart;
-    @BindView(R.id.btStop) Button btStop;
-    @BindView(R.id.mapView2) MapView mapView;
-    @BindView(R.id.progressBar2) ProgressBar pbLoading;
+    @BindView(R.id.btStart)
+    Button btStart;
+    @BindView(R.id.btStop)
+    Button btStop;
+    @BindView(R.id.mapView2)
+    MapView mapView;
+    @BindView(R.id.progressBar2)
+    ProgressBar pbLoading;
 
     @Nullable
     @Override
@@ -47,6 +55,7 @@ public class LiveRouteFragment extends Fragment implements OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
         fragmentManager = getFragmentManager();
+        context = getContext();
 
         mapView.onCreate(savedInstanceState);
         showProgressBar();
@@ -63,12 +72,11 @@ public class LiveRouteFragment extends Fragment implements OnMapReadyCallback {
         hideProgressBar();
         mGoogleMap = googleMap;
         mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-        
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mGoogleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        getPermissionToAccessLocation();
     }
+
+
 
     @Override
     public void onResume() {
@@ -94,5 +102,48 @@ public class LiveRouteFragment extends Fragment implements OnMapReadyCallback {
 
     private void hideProgressBar() {
         pbLoading.setVisibility(ProgressBar.INVISIBLE);
+    }
+
+    private void getPermissionToAccessLocation() {
+        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // The permission is NOT already granted.
+
+            // Check if the user has been asked about this permission already and denied
+            // it. If so, we want to give more explanation about why the permission is needed.
+            if (shouldShowRequestPermissionRationale(
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show our own UI to explain to the user why we need to read the contacts
+                // before actually requesting the permission and showing the default UI
+            }
+
+            // Fire off an async request to actually get the permission
+            // This will show the standard permission request dialog UI
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_FINE_LOCATION);
+        } else {
+            mGoogleMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == REQUEST_FINE_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(context, "Access fine location permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                // showRationale = false if user clicks Never Ask Again, otherwise true
+                boolean showRationale = shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION);
+
+                if (showRationale) {
+                    // do something here to handle degraded mode
+                    Toast.makeText(context, "do something here to handle degraded mode", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Access fine location permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
