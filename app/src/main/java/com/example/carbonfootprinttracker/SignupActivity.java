cfb3ce973bc7c8136ccda9236b254c456cd1eadf
acamacho4957 <acamacho4957@gmail.com>
2019-08-02 07:36:01 -0700

    Toast if username is taken, email is taken. Add progress bar to signup

--- app/src/main/java/com/example/carbonfootprinttracker/SignupActivity.java
+++ app/src/main/java/com/example/carbonfootprinttracker/SignupActivity.java
@@ -3,8 +3,10 @@ package com.example.carbonfootprinttracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -19,13 +21,14 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class SignupActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivty";
+    private static final String TAG = "SignupActivity";
 
     @BindView(R.id.etUsername) EditText etUsername;
     @BindView(R.id.etPassword) EditText etPassword;
     @BindView(R.id.etEmail) EditText etEmail;
     @BindView(R.id.etConfirmPassword) EditText etConfirmPassword;
     @BindView(R.id.btnSignup) Button btnSignup;
+    @BindView(R.id.progressBar4) ProgressBar pbLoading;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,22 +40,20 @@ public class SignupActivity extends AppCompatActivity {
 
     @OnClick(R.id.btnSignup)
     public void signup() {
+        pbLoading.setVisibility(View.VISIBLE);
 
         final String username = etUsername.getText().toString();
         final String password = etPassword.getText().toString();
         final String email = etEmail.getText().toString();
         final String confirmPassword = etConfirmPassword.getText().toString();
 
-        //check if username is empty, check if email is empty
-        if (username.isEmpty()){
+        if (username.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Missing Username!", Toast.LENGTH_SHORT).show();
-            return;}
-
-        if (email.isEmpty()){
+            return;
+        } else if (email.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Missing Email!", Toast.LENGTH_SHORT).show();
-            return;}
-
-        if (password.contentEquals(confirmPassword)) {
+            return;
+        } else if (password.contentEquals(confirmPassword)) {
             ParseUser user = new ParseUser();
             user.setUsername(username);
             user.setEmail(email);
@@ -61,14 +62,17 @@ public class SignupActivity extends AppCompatActivity {
             user.signUpInBackground(new SignUpCallback() {
                 @Override
                 public void done(ParseException e) {
+                    pbLoading.setVisibility(View.GONE);
                     if (e == null) {
                         Log.d(TAG, "Sign up successful!");
-
                         Intent i = new Intent(SignupActivity.this, OnBoardingActivity.class);
                         startActivity(i);
                         finish();
                     } else {
                         Log.d(TAG, "Sign up not successful");
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                        etPassword.setText("");
+                        etConfirmPassword.setText("");
                         e.printStackTrace();
                     }
                 }
