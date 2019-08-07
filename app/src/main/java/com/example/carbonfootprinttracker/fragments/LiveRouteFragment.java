eb8510ece4e5214dcdf506333034ed318a8b4670
Lisa Moshiro <lmoshiro@fb.com>
2019-08-07 12:54:09 -0700

    changed route and live route fragment titles

--- app/src/main/java/com/example/carbonfootprinttracker/fragments/LiveRouteFragment.java
+++ app/src/main/java/com/example/carbonfootprinttracker/fragments/LiveRouteFragment.java
@@ -102,7 +102,16 @@ public class LiveRouteFragment extends Fragment implements OnMapReadyCallback {
 
         try {
             carbie = getArguments().getParcelable("carbie");
-            ((MainActivity) getActivity()).getSupportActionBar().setTitle(carbie.getTransportation());
+            String transportation = carbie.getTransportation();
+            String title = "";
+            if (transportation.equals("FossilFuel") || transportation.equals("Renewable")) {
+                title = "Full Electric";
+            } else if (transportation.equals("SmallCar") || transportation.equals("MediumCar") || transportation.equals("LargeCar")) {
+                title = "Gasoline Car";
+            } else {
+                title = transportation;
+            }
+            ((MainActivity) getActivity()).getSupportActionBar().setTitle(title);
         } catch (NullPointerException e) {
             Log.e(TAG, "Carbie was not passed into LiveRouteFragment");
             e.printStackTrace();
